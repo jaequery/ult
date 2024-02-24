fb9c934d04a4ae2ad08aac1e319a53b29151b88b
Jae Lee <jaequery@gmail.com>
2024-02-24 10:41:32 -0800

    feat: add trpc exception handler (WIP) feat: add user login page chore: rename from Ultima to Ult Stack

--- apps/server/src/trpc/trpc.exception-handler.ts
+++ apps/server/src/trpc/trpc.exception-handler.ts
@@ -2,7 +2,7 @@ import { Catch, ArgumentsHost } from '@nestjs/common';
 import { BaseExceptionFilter } from '@nestjs/core';
 
 /**
- * TODO: Can't get this to work, see https://github.com/jaequery/Ultima/issues/1
+ * TODO: Can't get this to work, see https://github.com/jaequery/ult-stack/issues/1
  */
 @Catch()
 export class TrpcExceptionFilter extends BaseExceptionFilter {
