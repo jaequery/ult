dac5d385fcb4adf134aa93d096f4fd2dc5c26786
Jae Lee <jaequery@gmail.com>
2024-03-10 15:16:24 -0700

    chore: remove filter for now

--- apps/web/app/dashboard/users/DashboardUserList.tsx
+++ apps/web/app/dashboard/users/DashboardUserList.tsx
@@ -76,12 +76,6 @@ export default function DashboardUserList() {
                           Remove
                         </a>
                       )}
-                      <a
-                        className="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
-                        href="#"
-                      >
-                        Filter
-                      </a>
                       <Link
                         href="#"
                         onClick={() => {
