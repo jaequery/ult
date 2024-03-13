47541007aac22617e7dc0447c6c33bce900f6c62
Jae Lee <jaequery@gmail.com>
2024-03-12 21:56:43 -0700

    chore: add demo login user/pw

--- apps/web/app/login/Login.tsx
+++ apps/web/app/login/Login.tsx
@@ -36,6 +36,7 @@ export default function Login() {
           <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
             Sign in to your account
           </h2>
+          <p className="mt-4 text-center">Demo: admin@example.com / password</p>
         </div>
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
           <form
