9f16f78566d66d74aa1a1f1cdec638765789d6dd
Jae Lee <jaequery@gmail.com>
2024-10-27 23:18:22 -0700

    hotfix: remove duplicate db migration

--- apps/server/prisma/migrations/20241019210042_update_user_for_auth/migration.sql
+++ apps/server/prisma/migrations/20241019210042_update_user_for_auth/migration.sql
@@ -1,7 +1,3 @@
--- CreateEnum
-CREATE TYPE "AuthType" AS ENUM ('email', 'google');
-
 -- AlterTable
-ALTER TABLE "User" ADD COLUMN     "authType" "AuthType" NOT NULL DEFAULT 'email',
 ADD COLUMN     "lastLoggedInAt" TIMESTAMP(3),
 ADD COLUMN     "username" TEXT;
