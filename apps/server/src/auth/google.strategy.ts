c27eeea06dcbf6d9613e8ebfa259cd117afb72dd
Jae Lee <jaequery@gmail.com>
2025-09-03 02:07:39 -0700

    chore: upgrade nest v10 to v11

--- apps/server/src/auth/google.strategy.ts
+++ apps/server/src/auth/google.strategy.ts
@@ -11,9 +11,9 @@ export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
     private readonly configService: ConfigService,
   ) {
     super({
-      clientID: configService.get<string>('GOOGLE_CLIENT_ID'),
-      clientSecret: configService.get<string>('GOOGLE_CLIENT_SECRET'),
-      callbackURL: configService.get<string>('GOOGLE_CALLBACK_URL'),
+      clientID: configService.get<string>('GOOGLE_CLIENT_ID') || '',
+      clientSecret: configService.get<string>('GOOGLE_CLIENT_SECRET') || '',
+      callbackURL: configService.get<string>('GOOGLE_CALLBACK_URL') || '',
       scope: ['email', 'profile'],
     });
   }
